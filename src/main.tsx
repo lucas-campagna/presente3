654707e14e275733393d58313cb8e494d6d43f77
Your Name <you@example.com>
2025-09-18 10:26:25 -0300

    style: removing unecessary App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { Route, Routes } from "react-router";
+import Home from "./pages/home";
+import { SurrealProvider } from "./contexts/SurrealProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={client}>
+        <SurrealProvider
+          endpoint={"indxdb://presente"}
+          params={{ namespace: "presente", database: "dados" }}
+          autoConnect
+        >
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </SurrealProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
