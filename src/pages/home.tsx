3655fa3a55a982cc5f1bdaf046a3515d00de132e
Lucas Prett Campagna <lucasprett129@gmailcom>
2025-09-21 16:31:44 -0300

    fixup! fix: rename model class to classRoom to avoid syntax issues

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <ListView
       title={"Olá, estas são as suas turmas"}
-      menu={["add", "filter", "search", "archive", "report"]}
-      model={'class'}
+      menu={["add", "filter", "search", "archive", "report", "settings"]}
+      model={"classRoom"}
       onMenuClick={(type) => console.log(type)}
     />
   );
