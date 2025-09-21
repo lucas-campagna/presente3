007b29d8ae4deef833661d5d2984e36da6a64440
Lucas Prett Campagna <lucasprett129@gmailcom>
2025-09-21 16:42:40 -0300

    feat: navigate from home to attendance list if click on item

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router";
 import ListView from "../components/ListView";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <ListView
       title={"Olá, estas são as suas turmas"}
       menu={["add", "filter", "search", "archive", "report", "settings"]}
       model={"classRoom"}
       onMenuClick={(type) => console.log(type)}
+      onItemClick={(item) => navigate(`/attendance/${item.id.id}`)}
+      onItemOptionsClick={console.log}
     />
   );
 }
