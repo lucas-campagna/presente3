9db3d7db71660e7b977c8658b98f3355265065e7
Lucas Prett Campagna <lucasprett129@gmailcom>
2025-09-21 16:42:49 -0300

    feat: student model

--- src/models/student.ts
+++ src/models/student.ts
@@ -1,4 +1,14 @@
+import type { RecordId } from "surrealdb";
+
 export type Student = {
-  id: string;
+  id: RecordId<"student">;
   name: string;
 };
+
+export function getLabel(item: Student) {
+  return item.name;
+}
+
+export function onSort(a: Student, b: Student) {
+  return a.name.localeCompare(b.name);
+}
