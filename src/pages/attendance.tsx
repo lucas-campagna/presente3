9c9565cb843265fefde25c55d2f741abff181803
Lucas Prett Campagna <lucasprett129@gmailcom>
2025-09-21 16:38:26 -0300

    fixup! fix: change attendance class to id page param

--- src/pages/attendance.tsx
+++ src/pages/attendance.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router";
 import ListView from "../components/ListView";
 
 export default function Attendance() {
-  let { className } = useParams();
+  const { id } = useParams();
 
   return (
     <ListView
