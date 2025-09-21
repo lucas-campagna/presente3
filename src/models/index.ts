07837e00107cbba0653e528651f43495b556007f
Lucas Prett Campagna <lucasprett129@gmailcom>
2025-09-21 16:42:56 -0300

    fix: models exporting

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,12 @@
 import type { ClassRoom } from "./classRoom";
+import type { Attendance } from "./attendance";
+import type { Student } from "./student";
+export * as classRoom from "./classRoom";
+export * as attendance from "./attendance";
+export * as student from "./student";
 
 export type AvailableModels = {
-    class: Class;
-}
-
-export const getLabel = {
-    class: getLabelClass,
-}
-
-export const onSort = {
-    class: onSortClass,
-}
+  classRoom: ClassRoom;
+  attendance: Attendance;
+  student: Student;
+};
